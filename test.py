d6381550ad3e276f592b5a3c80a576c43af09e00
Amy <amyngph@gmail.com>
2021-02-11 06:53:28 -0800

    Side by side testing

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import vrProjector
-
-eq = vrProjector.EquirectangularProjection()
-eq.loadImage("images/equirectangular.png")
-# eq.set_use_bilinear(True)
-cb = vrProjector.CubemapProjection()
-cb.initImages(256,256)
-cb.reprojectToThis(eq)
-cb.saveImages("front.png", "right.png", "back.png", "left.png", "top.png", "bottom.png")
-
-
 #
-# sbs = SideBySideFisheyeProjection()
-# sbs.initImage(2048, 1024)
-## sbs.reprojectToThisThreaded(eq, 8)
+# eq = vrProjector.EquirectangularProjection()
+# # eq.loadImage("images/sidebysidefisheye.png")
+# # # eq.set_use_bilinear(True)
+# # cb = vrProjector.CubemapProjection()
+# eq.initImage(2048,1024)
+# # cb.reprojectToThis(eq)
+# # cb.saveImages("front.png", "right.png", "back.png", "left.png", "top.png", "bottom.png")
+#
+#
+#
+# sbs = vrProjector.SideBySideFisheyeProjection()
+# # sbs.initImage(2048, 1024)
+# sbs.loadImage("images/sidebysidefisheye.png")
+# # sbs.reprojectToThisThreaded(eq, 8)
 # sbs.reprojectToThis(eq)
 # sbs.saveImage("foo.png")
 #
@@ -33,9 +34,22 @@ cb.saveImages("front.png", "right.png", "back.png", "left.png", "top.png", "bott
 # cb.saveImages("front.png", "right.png", "back.png", "left.png", "top.png", "bottom.png")
 #
 
-# cb2 = CubemapProjection()
-# cb2.loadImages("front.png", "right.png", "back.png", "left.png", "top.png", "bottom.png")
-# eq2 = EquirectangularProjection()
+
+# cb2 = vrProjector.SideBySideFisheyeProjection()
+# cb2.loadImage("images/sidebysidefisheye.png", "images/right.png", "images/back.png", "images/left.png", "images/top.png", "images/bottom.png")
+# eq2 = vrProjector.EquirectangularProjection()
 # eq2.initImage(2048,1024)
 # eq2.reprojectToThis(cb2)
 # eq2.saveImage("foo.png")
+print("Here1")
+cb2 = vrProjector.SideBySideFisheyeProjection()
+print("Here2")
+cb2.loadImage("images/side-by-side-sketch.png")
+print("Here3")
+eq2 = vrProjector.EquirectangularProjection()
+print("Here4")
+eq2.initImage(2048,1024)
+print("Here5")
+eq2.reprojectToThis(cb2)
+print("Here6")
+eq2.saveImage("foo.png")
